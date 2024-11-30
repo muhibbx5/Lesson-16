27b2eaa1f96d0e7bd86fb4684191d77e3de1050d
muhibbx5 <muhibbali56@gmail.com>
2024-11-30 13:46:09 +0500

    commit

---
+++ activity2.py
@@ -0,0 +1,11 @@
+x=int(input("Please enter a number: "))
+ 
+if x%20==0: 
+    print("twist")
+elif x % 15==0:
+
+    print("pass")
+elif x % 5==0:
+    print("fizz")
+elif x % 5==0:
+    print("buzz")
---
+++ main.py
@@ -0,0 +1,8 @@
+a=input("enter a word : ")
+for i in a: 
+    if (i=="a"):
+       print("Found the letter a ")
+
+       break 
+else:
+    print("a not found")
